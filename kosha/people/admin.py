52a745e5eea41aefdfae2411c4b348cf8f4e0093
Ratnadeep Debnath <rtnpro@gmail.com>
2019-07-07 15:19:58 +0530

    Customize djangoql static files to make it work with django-jet.

--- kosha/people/admin.py
+++ kosha/people/admin.py
@@ -12,7 +12,7 @@ class MeetingInline(admin.StackedInline):
 
 
 @admin.register(Person)
-class PersonAdmin(VersionAdmin):
+class PersonAdmin(DjangoQLSearchMixin, VersionAdmin):
     list_display = (
         "name",
         "reference_number",
